3f1e7a4220dc4eb60428b590a03a5ee7651676a9
nld <nld@pad.localdomain>
2021-08-06 13:34:07 +0200

    patched to look like bar

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMonoMedium Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -15,6 +15,9 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 8;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
